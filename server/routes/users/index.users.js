84b0e451c8e1a632d3d908cf0f24f1f6dee1905e
Todd Kronenberg <toddkronenberg@gmail.com>
2015-12-21 22:08:10 -0600

    Trying to figure out unit tests, not yet though :(

--- server/routes/users/index.users.js
+++ server/routes/users/index.users.js
@@ -7,6 +7,6 @@ var User = require('./../../models/user.model');
 module.exports = function(req, res) {
   User.index(function(err, records) {
     if (err) return res.send(err).status(500);
-    return res.status(200).send(records);
+    return res.send(records);
   });
 };
--- test/unit/users.unit.test.js
+++ test/unit/users.unit.test.js
@@ -2,21 +2,58 @@
 
 var assert = require('chai').assert;
 var sinon = require('sinon');
-var http = require('http');
-var port = require('./../../server/config/config').test.port;
-var app = require('../../server/app');
+var User = require('./../../server/models/user.model');
+var userIndex = require('./../../server/routes/users/index.users');
 
 
 describe('Users', function() {
 
-  before(function(done) {
-    app.start(done);
+  // before(function(done) {
+    // app.start(done);
+
+  // });
+
+  describe('get /users', function() {
+    var req, res, statusCode, sentData;
+    res = {
+      send: function(code, data) {
+        statusCode = code;
+        sentData = data;
+      }
+    };
+
+    it('should return all names from database', function(done) {
+      
+      // console.log('folder', userIndex);
+      // var stub = sinon.stub(UserIndex).;
+      // var req = {}, res = {};
+      // var spy = res.send = sinon.spy();
+      // userIndex(req, res);
+      // assert.equal(spy.callCount, 1);
+      // assert.equal(spy.calledOnce, true);
+      // var stub = sinon.stub(User.index);
+      // var callback = stub.returnsArg(0);
+      // User.prototype.
+      // userIndex(req, res);
+      // console.log('cb', callback);
+      // callback(err, records)
+      // userIndex(function() {
+
+      // });
+      // stub.restore();
+      // done();
+    });
   });
 
+  // describe('get /users', function() {
+  //   it('should return an error message and status 500', function(done) {
+  //     sinon.stub(User, 'index'.)
+  //   });
+  // });
 
 
-  after(function(done) {
-    app.stop(done);
-  });
+  // after(function(done) {
+    // app.stop(done);
+  // });
 
 });
